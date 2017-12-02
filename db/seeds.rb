b57764c45a132916018b7a9cac2dd6846a9dd39b
João Marcus de Lemos Fernandes <jmarcusfernandes@gmail.com>
2017-12-02 19:43:06 -0200

    Added more books to the seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 Product.delete_all
 Product.create! id: 1, name: "Learn RoR - Beginner", price: 24.99
 Product.create! id: 2, name: "Mastering RoR - Level over 9000", price: 9001.00
+Product.create! id: 3, name: "30 Days of Elixir", price: 10.00
+Product.create! id: 4, name: "Joy of Elixir", price: 6.00
+Product.create! id: 5, name: "Clojure - Functional Programming for the JVM", price: 15.55
+Product.create! id: 6, name: "ClojureScript Unraveled", price: 80.00
+Product.create! id: 7, name: "Exploring ES6", price: 25.00
+Product.create! id: 8, name: "JavaScript Garden", price: 50.30
+Product.create! id: 9, name: "Just Enough Ruby to Get By", price: 23.05
