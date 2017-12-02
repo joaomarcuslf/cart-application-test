6b0410738c78190325306dacc5c02b3f2aa33264
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2017-12-02 19:05:16 -0200

    Added cart controller

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,53 @@
+class CartController < ApplicationController
+  include CartHelper
+  before_action :get_valid_cart, only: [:index, :update]
+  before_action :filter_params, only: [:update_cart]
+
+  def index
+  end
+
+  def update
+  end
+
+  def update_cart
+    products = params[:products].to_h
+
+    ids = products.keys
+
+    cart = session[:cart]
+
+    cart['size'] = 0
+
+    ids.each do |id|
+      quant = products[id]["'quant'"].to_i
+
+      if quant > 0 then
+        cart[id]['quant'] = quant
+      else
+        cart.delete(id)
+      end
+
+      cart['size'] += quant
+    end
+
+
+    session[:cart] = cart
+
+    redirect_to cart_path
+  end
+
+  def empty
+    empty_cart!()
+
+    redirect_to root_path
+  end
+
+  private
+    def filter_params
+      params.require(:products).permit!
+    end
+
+    def get_valid_cart
+      @cart = format_cart()
+    end
+end
---
+++ spec/controllers/cart_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe CartController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #empty" do
+    it "returns http success" do
+      get :empty
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #update" do
+    it "returns http success" do
+      get :update
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
