4beb5e0ca60487ab8af7e6f54ee425b0e44dcd39
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2017-12-03 13:46:23 -0200

    Implement the check price change

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,4 +1,5 @@
 class CatalogController < ApplicationController
+  include ApplicationHelper
   before_action :filter_params, only: [:product]
 
   def home
@@ -7,32 +8,41 @@ class CatalogController < ApplicationController
 
   def product
     id = params[:id]
-    add_to_cart(id)
+    add_to_cart!(id)
+
+    flash[:notice] = "Product added to your cart!"
 
     redirect_to :home
   end
 
   private
-    def add_to_cart(product)
+    def add_to_cart!(product)
+      # The cart must store the total amount of
+      # products stored, the total amount of a singular product
+      # and, if the cart is valid, the total of all products
+      # this is created on cart_controller
+
       id = product.to_s
 
-      cart = session[:cart]
+      old_cart = session[:cart]
 
-      cart ||= {
-        'size' => 0
-      }
+      old_cart ||= get_empty_cart()
+      old_cart[id] ||= get_empty_product(id)
 
-      if !cart[id] then
-        cart[id] = {
-          'quant' => 0,
-          'id' => id
-        }
+      begin
+        formatted_product = get_valid_product(old_cart, id)
+        formatted_product['quant']+=1
+        old_cart[id] = formatted_product
+      rescue => e
+        puts e
+        return e
       end
 
-      cart['size']+=1
-      cart[id]['quant']+=1
+      old_cart['size']+=1
 
-      session[:cart] = cart
+      old_cart.delete('total')
+
+      session[:cart] = old_cart
     end
 
     def filter_params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 module ApplicationHelper
+  def get_empty_cart
+    return {
+      'size' => 0,
+      'total' => 0
+    }
+  end
+
+  def get_empty_product(id)
+    return {
+      'quant' => 0,
+      'id' => id,
+      'total' => 0,
+      'name' => '',
+      'price' => 0.0,
+    }
+  end
+
   def cart_size
     if session[:cart] && session[:cart]['size'] then
       session[:cart]['size']
@@ -6,4 +23,25 @@ module ApplicationHelper
       0
     end
   end
+
+  def format_product(product_instance, quant)
+    product = {
+      'id' => product_instance.id,
+      'name' => product_instance.name,
+      'price' => product_instance.price,
+      'quant' => quant,
+      'total' => product_instance.price  * quant
+    }
+
+    return product
+  end
+end
+
+def get_valid_product(cart, id)
+  product = Product.find(id)
+
+  cart[id]['quant'] ||= 1
+  product_quant = cart[id]['quant'].to_i
+
+  formatted_product = format_product(product, product_quant)
 end
